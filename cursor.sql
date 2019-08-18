34ce31102b5a137edd5e0d73c1bd23df2cad9af7
justine05 <justineb@cet.ac.in>
2019-08-18 21:20:41 +0530

    Added plsql and cursor

--- cursor.sql
+++ cursor.sql
@@ -37,3 +37,32 @@ BEGIN
     RETURN 0;
 END;
 $$ LANGUAGE plpgsql;
+
+--second program
+
+insert into bank_details values(1001,'aby',3005,'10-oct-15');
+insert into bank_details values(1002,'alan',4000,'05-may-95');
+insert into bank_details values(1003,'amal',5000,'16-mar-92');
+insert into bank_details values(1004,'jeffin',3500,'01-apr-50');
+insert into bank_details values(1005,'majo',6600,'01-jan-01');
+
+CREATE OR REPLACE FUNCTION cal_int() RETURNS INTEGER AS $$
+DECLARE
+    c1 CURSOR FOR SELECT * FROM bank_details;
+    rec RECORD;
+BEGIN
+    OPEN c1;
+    LOOP
+        FETCH c1 INTO rec;
+        EXIT WHEN NOT FOUND;
+        INSERT INTO bank_new VALUES (rec.accno, rec.balance*0.08);
+    END LOOP;      
+    CLOSE c1;  
+    RETURN 0;
+END;
+$$ LANGUAGE plpgsql;
+
+--third program
+
+create table people_list(id INT, name varchar(20),dt_joining DATE,place varchar(20));
+create table exp_list(id INT, name varchar(20),exp INT);
